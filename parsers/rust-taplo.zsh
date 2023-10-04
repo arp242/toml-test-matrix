e17226cf9b475e2d24101872aace80d03c9cb5c4
Ed Page <eopage@gmail.com>
2023-10-04 14:49:12 -0500

    Add rust-taplo decoder

---
+++ parsers/rust-taplo.zsh
@@ -0,0 +1,15 @@
+setup() {
+	cd scripts
+	cargo build --release
+}
+
+typeset -A info=(
+	lang    'Rust'
+	toml    '1.0'
+	site    'https://github.com/tamasfe/taplo/tree/master/crates/taplo'
+	src     '' # We use the Cargo.toml for now 'git@github.com:tamasfes/taplo.git'
+	version '0.12.1'
+	decoder './scripts/target/release/taplo-decoder'
+	encoder './scripts/target/release/taplo-encoder'
+	perf    './scripts/target/release/rust-taplo-perf'
+)
