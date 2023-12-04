0b723823e5256a30b31f81ad75bbb1ae23bb9aa4
Martin Tournoij <martin@arp242.net>
2023-12-04 02:23:51 +0000

    Add ocaml-otoml

--- parsers/ocaml-otoml.zsh
+++ parsers/ocaml-otoml.zsh
@@ -1,15 +1,18 @@
 setup() {
-	:
+	eval $(opam env)
+	opam -y pin --with-test add .
+
+	opam install -y yojson zarith decimal ocamlfind
+	ocamlfind ocamlopt -package otoml,zarith,decimal,yojson -linkpkg ./examples/testsuite_client_decoder.ml -o decoder
+	ocamlfind ocamlopt -package otoml,zarith,decimal,yojson -linkpkg ./examples/testsuite_client_encoder.ml -o encoder
 }
 
 typeset -A info=(
-	skip    '1' # TODO
-
 	lang    'OCAML'
 	toml    '1.0'
 	site    'https://github.com/dmbaturin/otoml'
 	src     'git@github.com:dmbaturin/otoml.git'
-	decoder ''
-	encoder 'TODO'
+	decoder 'decoder'
+	encoder 'encoder'
 	perf    ''
 )
