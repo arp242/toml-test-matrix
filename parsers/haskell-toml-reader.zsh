2db25d878df2f3bd452fefb574c07fab1dd71d73
Martin Tournoij <martin@arp242.net>
2023-12-04 01:37:02 +0000

    Add haskell-toml-reader

--- parsers/haskell-toml-reader.zsh
+++ parsers/haskell-toml-reader.zsh
@@ -1,18 +1,20 @@
 setup() {
+	# Note: Needs cabal 3.10 (or 3.8?) for --prefer oldest. 3.6 is too old at least.
 	cabal update
-	cabal configure --enable-test
+	cabal configure --enable-test --prefer-oldest
 	cabal build
-	cabal test
+
+	# What a path... Copy it to something vaguely managable.
+	# dist-newstyle/build/x86_64-linux/ghc-9.4.7/toml-reader-0.2.1.0/t/parser-validator/build/parser-validator/parser-validator .
+	cp dist-newstyle/build/x86_64-linux/ghc-*/toml-reader-*/t/parser-validator/build/parser-validator/parser-validator .
 }
 
 typeset -A info=(
-	# TODO
-    skip    '1'
 	lang    'Haskell'
 	toml    '1.0'
 	site    'https://github.com/brandonchinn178/toml-reader'
 	src     'git@github.com:brandonchinn178/toml-reader.git'
-	decoder ''
+	decoder './src/haskell-toml-reader/parser-validator --check'
 	encoder ''
 	perf    'TODO'
 )
