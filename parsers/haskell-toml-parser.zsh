547f6a90f2f2945c848235be50602984778184af
Martin Tournoij <martin@arp242.net>
2023-12-11 05:29:09 +0000

    Add benchmarks for haskell-toml-parser

--- parsers/haskell-toml-parser.zsh
+++ parsers/haskell-toml-parser.zsh
@@ -3,6 +3,9 @@ deps() { print 'cabal' }
 setup() {
 	cabal update
 
+	grep 'buildable: False' &&
+		sed -i.orig 's/buildable: False/buildable: True/' toml-parser.cabal
+
 	cd test-drivers
 	cabal configure
 	cabal build
@@ -11,9 +14,10 @@ setup() {
 	# What a path... Copy it to something vaguely managable.
 	# dist-newstyle/build/x86_64-linux/ghc-9.4.7/toml-test-drivers-1.0.0.0/x/TomlDecoder/build/TomlDecoder/TomlDecoder*
 	# dist-newstyle/build/x86_64-linux/ghc-9.4.7/toml-test-drivers-1.0.0.0/x/TomlEncoder/build/TomlEncoder/TomlEncoder*
-
+	# dist-newstyle/build/x86_64-linux/ghc-9.4.7/toml-parser-1.3.1.1/x/toml-benchmarker/build/toml-benchmarker/toml-benchmarker*
 	cp dist-newstyle/build/x86_64-linux/ghc-*/toml-test-drivers-*/x/TomlDecoder/build/TomlDecoder/TomlDecoder .
 	cp dist-newstyle/build/x86_64-linux/ghc-*/toml-test-drivers-*/x/TomlEncoder/build/TomlEncoder/TomlEncoder .
+	cp dist-newstyle/build/x86_64-linux/ghc-*/toml-parser-*/x/toml-benchmarker/build/toml-benchmarker/toml-benchmarker .
 }
 
 typeset -A info=(
@@ -23,5 +27,5 @@ typeset -A info=(
 	src     'git@github.com:glguy/toml-parser.git'
 	decoder 'TomlDecoder'
 	encoder 'TomlEncoder'
-	perf    'TODO'
+	perf    'toml-benchmarker'
 )
--- parsers/haskell-toml-reader.zsh
+++ parsers/haskell-toml-reader.zsh
@@ -1,6 +1,35 @@
 deps() { print 'cabal' }
 
 setup() {
+	grep 'executable perf' toml-reader.cabal || cat >>toml-reader.cabal <<-EOF
+		executable perf
+		    buildable: True
+		    main-is: perf.hs
+		    default-language: Haskell2010
+		    build-depends: base, toml-reader, time
+		    hs-source-dirs: perf
+	EOF
+	mkdir -p perf
+	cat >|perf/perf.hs <<-'EOF'
+		import Control.Exception (evaluate)
+		import Data.Time (diffUTCTime, getCurrentTime)
+		import System.Environment (getArgs)
+		import TOML.Parser (parseTOML)
+
+		main :: IO ()
+		main =
+		 do args <- getArgs
+		    filename <- case args of
+		      [filename] -> pure filename
+		      _ -> fail "Usage: perf <file.toml>"
+		    txt <- readFile filename
+		    evaluate (length txt) -- readFile uses lazy IO, force it to load
+		    start <- getCurrentTime
+		    evaluate (parseTOML txt)
+		    stop <- getCurrentTime
+		    print (stop `diffUTCTime` start)
+	EOF
+
 	# Note: Needs cabal 3.10 (or 3.8?) for --prefer oldest. 3.6 is too old at least.
 	cabal update
 	cabal configure --enable-test --prefer-oldest
@@ -8,7 +37,9 @@ setup() {
 
 	# What a path... Copy it to something vaguely managable.
 	# dist-newstyle/build/x86_64-linux/ghc-9.4.7/toml-reader-0.2.1.0/t/parser-validator/build/parser-validator/parser-validator .
+	# dist-newstyle/build/x86_64-linux/ghc-9.4.7/toml-reader-0.2.1.0/x/perf/build/perf/perf
 	cp dist-newstyle/build/x86_64-linux/ghc-*/toml-reader-*/t/parser-validator/build/parser-validator/parser-validator .
+	cp dist-newstyle/build/x86_64-linux/ghc-*/toml-reader-*/x/perf/build/perf/perf perf-bin
 }
 
 typeset -A info=(
@@ -18,5 +49,5 @@ typeset -A info=(
 	src     'git@github.com:brandonchinn178/toml-reader.git'
 	decoder './src/haskell-toml-reader/parser-validator --check'
 	encoder ''
-	perf    'TODO'
+	perf    'perf-bin'
 )
